02dfd137016c564eb18cf878723dcf1e0cfb851d
worrior9119 <theemperorakshay@gmail.com>
2020-09-07 18:49:42 +0530

    Practice commit

---
+++ DirPath.java
@@ -0,0 +1,26 @@
+package com.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class DirPath {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(dirPath(str));
+    }
+    public static String dirPath(String str){
+        Stack<String> s = new Stack<String>();
+        String[] p = str.split("/");
+        for (String t: p){
+            if(!t.isEmpty() && t.equals(".."))
+                s.pop();
+            else if(!t.equals(".")&&!t.equals("")&&!t.equals(".."))
+                s.push(t);
+        }
+        List<String> res = new ArrayList<>(s);
+        return "/"+String.join("/",res);
+    }
+}
